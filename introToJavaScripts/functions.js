ea679efbed6c79d4a9b1bffdc0640eebb42974a6
Marcin Kosciolek <marcin.kosciolek@netent.com>
2018-01-05 10:26:53 +0100

    Added example to modification

---
+++ introToJavaScripts/functions.js
@@ -0,0 +1,22 @@
+var str = "";
+for (let i = 0; i<10; i++) {
+    str += "-";
+}
+console.log(str);
+
+console.log("20 + 20 = ", 20 + 20);
+
+var str = "";
+for (let i = 0; i<10; i++) {
+    str += "-";
+}
+console.log(str);
+
+console.log("Ala ma kota");
+console.log("a kot ma Alę");
+
+var str = "";
+for (let i = 0; i<10; i++) {
+    str += "-";
+}
+console.log(str);
